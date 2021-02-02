b37c0c710d01eb4f57440572d36413fc2b87f41b
alexandr.shevchenko <alexandr.shevchenko@thermofisher.com>
2021-02-02 12:28:26 +0200

    Add better data base

--- Demo/BetterDataBase.cs
+++ Demo/BetterDataBase.cs
@@ -2,18 +2,18 @@
 
 namespace Problem
 {
-    public class GoodDataBase
+    public class BetterDataBase
     {
         public DemoObject GetObject()
         {
-            Console.WriteLine("Getting the object");
+            Console.WriteLine("Getting the object faster");
             return new DemoObject();
         }
         
         public void SaveObject(DemoObject objectToSave)
         {
             objectToSave.NeedUpdate = false;
-            Console.WriteLine("Saving the object");
+            Console.WriteLine("Saving the object faster");
         }
     }
 }
