4ae98f80ffd980a168de4c69789cd380daa13b9d
alexandr.shevchenko <alexandr.shevchenko@thermofisher.com>
2021-02-02 15:54:00 +0200

    Add DI examples

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Autofac;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
 
 namespace Problem
 {
@@ -6,8 +9,36 @@ namespace Problem
     {
         static void Main(string[] args)
         {
+            //StartSimple();
+            //StartWindsor();
+            StartAutoFac();
+            Console.ReadKey();
+        }
+
+        static void StartSimple()
+        {
             var someBl = new BusinessLogic(new GoodDataBase());
             someBl.DoSomeActions();
+        }
+
+        static void StartWindsor()
+        {
+            var container = new WindsorContainer();
+            container.Register(Component.For<BusinessLogic>());
+            container.Register(Component.For<IDataBase>().ImplementedBy<BetterDataBase>()
+                .LifestyleSingleton());
+            var bl = container.Resolve<BusinessLogic>();
+            bl.DoSomeActions();
+        }
+
+        static void StartAutoFac()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<BusinessLogic>().AsSelf();
+            builder.RegisterType<BetterDataBase>().As<IDataBase>().SingleInstance();
+            var container = builder.Build();
+            var bl = container.Resolve<BusinessLogic>();
+            bl.DoSomeActions();
         }
     }
 }
